e4cee091a26f10586516f02f49ac5bc9afb3ee3b
George Kharchenko <hkharc2@uic.edu>
2019-12-15 21:53:53 -0600

    Add day 13 pt 2 (self-playing :-))

--- src/p13.ml
+++ src/p13.ml
@@ -1,5 +1,65 @@
 open Core
 
+let xaddr = 388
+let xaccaddr = 390
+let yaddr = 389
+let yaccaddr = 391
+
+let bottom_boundary = 20
+
+let width = 44
+let height = 24
+
+type pos = int * int
+let x p = fst p
+let y p = snd p
+let pos_to_string ((x,y): pos) = sprintf "%d,%d" x y
+let pos_of_string s =
+    match String.split ~on:',' s with
+    | [x;y] -> (Int.of_string x), (Int.of_string y)
+    | _ -> failwith "Malformed pos"
+
+type jpos = Neutral | Left | Right
+let jpos_to_int j =
+    match j with
+    | Neutral -> 0
+    | Left -> -1
+    | Right -> 1
+let jpos_of_char c =
+    match c with
+    | Some 'a' -> Left
+    | Some 'd' -> Right
+    | _ -> Neutral
+
+type tile =
+    | Empty
+    | Wall
+    | Block
+    | Paddle
+    | Ball
+let tile_of_int i =
+    match i with
+    | 0 -> Empty
+    | 1 -> Wall
+    | 2 -> Block
+    | 3 -> Paddle
+    | 4 -> Ball
+    | _ -> failwith "Invalid tile"
+let tile_to_int t =
+    match t with
+    | Empty -> 0
+    | Wall -> 1
+    | Block -> 2
+    | Paddle -> 3
+    | Ball -> 4
+let tile_to_string t =
+    match t with
+    | Empty -> " "
+    | Wall -> "|"
+    | Block -> "#"
+    | Paddle -> "="
+    | Ball -> "o"
+
 let input = ref 0
 let output = ref 0
 
@@ -27,6 +87,7 @@ type op =
     | Halt
 [@@deriving show]
 
+let last_read_val = ref 0
 let read (arg, mode) tape rb =
     match mode with
     | Addr -> tape.(arg)
@@ -107,126 +168,122 @@ let parse (tape: int array) (i: int): op * int =
     | _, _, _, 99 -> Halt, (i + 1)
     | a, b, c, d -> raise (IllegalOpCode (a, b, c, d))
 
-let rec eval (tape: int array) (ic: int) (rb: int) (halt_on_input: bool) (halt_on_output: bool): int * int * bool =
+
+let get1char () =
+    let open Unix.Terminal_io in
+    let termio = tcgetattr Unix.stdin in
+    let () =
+        tcsetattr { termio with c_icanon = false } Unix.stdin TCSADRAIN
+    in
+    let res = In_channel.input_char In_channel.stdin in
+    tcsetattr termio Unix.stdin TCSADRAIN;
+    res
+
+let paused = ref false
+let input_loop () =
+    while true do
+        let input = get1char () in
+        match input with
+        | Some 'c' -> paused := true
+        | Some 'v' -> paused := false
+        | _ -> ()
+    done
+
+let image =
+    let im = Array.create ~len:height [||] in
+    let y = ref 0 in
+    while !y <> height do
+        im.(!y) <- (Array.create ~len:width "+");
+        incr y
+    done;
+    im
+
+type game_state = X | Y | Tile
+let x = ref 0
+let y = ref 0
+let score = ref 0
+
+let draw () =
+    printf "Score: %d\n%s\n" (!score) ((image |> Array.map ~f:(fun line -> line |> Array.to_list |> String.concat)) |> Array.to_list |> String.concat ~sep:"\n");
+    Out_channel.flush stdout
+
+let is = ref 0
+let rec eval (tape: int array) (ic: int) (rb: int) (s: game_state): int * int * bool =
+    (* draw (); *)
+    if (tape.(yaddr) >= bottom_boundary) then (
+        (* Hehe :-) *)
+        tape.(yaccaddr) <- if tape.(yaccaddr) = 1 then -1 else 1
+    );
     let op, ic' = parse tape ic in
     match op with
     | Add (a, b, (dest, dm)) -> (
         let a = read a tape rb in
         let b = read b tape rb in
         write (a + b)  (dest, dm) tape rb;
-        eval tape ic' rb halt_on_input halt_on_output
+        eval tape ic' rb s
     )
     | Mul (a, b, (dest, dm)) -> (
         let a = read a tape rb in
         let b = read b tape rb in
         write (a * b)  (dest, dm) tape rb;
-        eval tape ic' rb halt_on_input halt_on_output
+        eval tape ic' rb s
     )
     | Lt (a, b, (dest, dm)) -> (
         let a = read a tape rb in
         let b = read b tape rb in
         write (if a < b then 1 else 0)  (dest, dm) tape rb;
-        eval tape ic' rb halt_on_input halt_on_output
+        eval tape ic' rb s
     )
     | Eq (a, b, (dest, dm)) -> (
         let a = read a tape rb in
         let b = read b tape rb in
         write (if a = b then 1 else 0)  (dest, dm) tape rb;
-        eval tape ic' rb halt_on_input halt_on_output
+        eval tape ic' rb s
     )
     | Input dest -> (
-        match halt_on_input with
-        | true -> ic, rb, false
-        | false -> (
-            write !input dest tape rb;
-            eval tape ic' rb halt_on_input halt_on_output
-        )
+        incr is;
+        if !paused then (
+            input := get1char () |> jpos_of_char |> jpos_to_int;
+        ) else input := Neutral |> jpos_to_int;
+
+        write !input dest tape rb;
+        eval tape ic' rb s
     )
     | Output a -> (
         let a = read a tape rb in
+        let s' = match s with
+            | X -> (x := a; Y)
+            | Y -> (y := a; Tile)
+            | Tile -> (
+                match !x, !y with
+                | -1, 0 -> score := a; X
+                | x, y -> image.(y).(x) <- (a |> tile_of_int |> tile_to_string); X
+            ) in
         output := a;
-        match halt_on_output with
-        | true -> ic', rb, false
-        | false -> eval tape ic' rb halt_on_input halt_on_output
+        eval tape ic' rb s'
     )
     | JumpIfTrue (test, dest) -> (
         let test = read test tape rb in
         let dest = read dest tape rb in
         match test with
-        | 0 -> eval tape ic' rb halt_on_input halt_on_output
-        | _ -> eval tape dest rb halt_on_input halt_on_output
+        | 0 -> eval tape ic' rb s
+        | _ -> eval tape dest rb s
     )
     | JumpIfFalse (test, dest) -> (
         let test = read test tape rb in
         let dest = read dest tape rb in
         match test with
-        | 0 -> eval tape dest rb halt_on_input halt_on_output
-        | _ -> eval tape ic' rb halt_on_input halt_on_output
+        | 0 -> eval tape dest rb s
+        | _ -> eval tape ic' rb s
     )
     | Rbo offset -> (
         let offset = read offset tape rb in
-        eval tape ic' (rb + offset) halt_on_input halt_on_output
+        eval tape ic' (rb + offset) s
     )
     | Halt -> ic, rb, true
-
-type tile =
-    | Empty
-    | Wall
-    | Block
-    | Paddle
-    | Ball
-let tile_of_int i =
-    match i with
-    | 0 -> Empty
-    | 1 -> Wall
-    | 2 -> Block
-    | 3 -> Paddle
-    | 4 -> Ball
-    | _ -> failwith "Invalid tile"
-let tile_to_int t =
-    match t with
-    | Empty -> 0
-    | Wall -> 1
-    | Block -> 2
-    | Paddle -> 3
-    | Ball -> 4
-
-type pos = int * int
-let x p = fst p
-let y p = snd p
-let pos_to_string ((x,y): pos) = sprintf "%d,%d" x y
-
-let set tbl k v =
-    (match Hashtbl.find tbl k with
-    | Some _ -> 
-        let _ = Hashtbl.remove tbl k in ()
-    | None -> ());
-    let _ = Hashtbl.add tbl k v in ()
-
-let run (tape: int array) =
-    let ic = ref 0 in
-    let rb = ref 0 in
-    let halted = ref false in
-    let pixels = Hashtbl.create (module String) in
-    while not !halted do
-        let (ic', rb', hlt) = eval tape !ic !rb false true in
-        let x = !output in
-        let (ic', rb', hlt) = eval tape ic' rb' false true in
-        let y = !output in
-        let (ic', rb', hlt) = eval tape ic' rb' false true in
-        let tid = !output in
-        ic := ic';
-        rb := rb';
-        halted := hlt;
-        set pixels (pos_to_string (x,y)) (tile_of_int tid)
-    done;
-    Hashtbl.to_alist pixels |> List.filter ~f:(fun (_, t) -> (
-        match t with
-        | Block -> true
-        | _ -> false
-    )) |> List.length |> printf "%d\n"
 
 let solve () =
     let prog = In_channel.read_all "inputs/13.txt" in
     let tape = make_tape prog in
-    run tape
+    (* let il = Thread.create input_loop () in *)
+    let _, _, _ = eval tape 0 0 X in printf "%d\n" !score
